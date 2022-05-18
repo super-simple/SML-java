be1c2aa1efd77fe663c088cc73a4da11dfc31a3b
david <david@abc.com>
2022-05-18 18:44:37 +0800

    不再使用antlr解析

--- src/main/java/org/ss/smljava/parse/SmlParser.java
+++ src/main/java/org/ss/smljava/parse/SmlParser.java
@@ -1,4 +1,16 @@
 package org.ss.smljava.parse;
 
+import org.ss.smljava.bo.SmlDocument;
+
 public class SmlParser {
+
+    public SmlDocument parseDocument(String documentStr) {
+        SmlDocument smlDocument = new SmlDocument();
+
+        return smlDocument;
+    }
+
+    public void parseHeader(String documentStr, SmlDocument smlDocument) {
+
+    }
 }
