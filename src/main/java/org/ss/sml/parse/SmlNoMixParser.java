111910683bfb31b54ddf4b7a5a0c5af9de8bff3e
David.Ricardo <david.lee.ricardo1026@gmail.com>
2022-08-05 11:31:20 +0800

    refactor to lexer and parser

--- src/main/java/org/ss/sml/parse/SmlNoMixParser.java
+++ src/main/java/org/ss/sml/parse/SmlNoMixParser.java
@@ -2,17 +2,12 @@ package org.ss.sml.parse;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.ss.sml.bo.MutableInt;
 import org.ss.sml.bo.TokenTrait;
 import org.ss.sml.exceptionclz.SmlBug;
-import org.ss.sml.exceptionclz.SmlErrorMessage;
 import org.ss.sml.exceptionclz.SmlFormatException;
 import org.ss.sml.util.ObjectMappers;
-
-import static org.ss.sml.exceptionclz.SmlErrorMessage.*;
-import static org.ss.sml.parse.SmlDelimiter.*;
 
 public class SmlNoMixParser {
 
@@ -42,44 +37,7 @@ public class SmlNoMixParser {
     }
 
     private static ObjectNode parseHeader(String smlStr, MutableInt indexHolder, int length, StringBuilder sb) {
-        ObjectNode objectNode = OBJECT_MAPPER.createObjectNode();
-        int index = indexHolder.getValue();
-        char c0 = 0;
-        int count = 0;
-        while (index < length) {
-            c0 = smlStr.charAt(index++);
-            if (!Character.isWhitespace(c0)) {
-                sb.append(c0);
-                count++;
-            } else {
-                if (count != 0) {
-                    break;
-                }
-            }
-        }
-        String sml = sb.toString();
-        if (!SmlDelimiter.SML.equals(sml)) {
-            throw new SmlFormatException(SmlErrorMessage.NOT_START_WITH_SML);
-        }
-        sb.delete(0, count);
-        indexHolder.setValue(index);
-
-        // find (
-        while (index < length) {
-            c0 = smlStr.charAt(index++);
-            if (c0 == LEFT_PARENTHESIS) {
-                break;
-            }
-            if (!Character.isWhitespace(c0)) {
-                throw new SmlFormatException(EXCEPT_LEFT_PARENTHESIS);
-            }
-        }
-        if (c0 != LEFT_PARENTHESIS) {
-            throw new SmlFormatException(EXCEPT_LEFT_PARENTHESIS);
-        }
-        indexHolder.setValue(index);
-        exceptAttribute(smlStr, indexHolder, length, sb, objectNode);
-        return objectNode;
+        return null;
     }
 
     private static char readContext(String smlStr, MutableInt indexHolder, int length, StringBuilder sb, TokenTrait tokenTrait) {
@@ -112,188 +70,7 @@ public class SmlNoMixParser {
     }
 
     private static ObjectNode parseBody(String smlStr, MutableInt indexHolder, int length, StringBuilder sb) {
-        int model = exceptElementName(smlStr, indexHolder, length, sb);
-        ObjectNode rootNode = OBJECT_MAPPER.createObjectNode();
-        String elementName = sb.toString();
-        sb.delete(0, sb.length());
-        if (model < 3) {
-            ObjectNode firstNode = OBJECT_MAPPER.createObjectNode();
-            rootNode.set(elementName, firstNode);
-            if (model == 1) {
-                exceptAttribute(smlStr, indexHolder, length, sb, firstNode);
-            }
-            exceptObjectElementValue(smlStr, indexHolder, length, sb, firstNode);
-        } else {
-            ArrayNode firstNode = OBJECT_MAPPER.createArrayNode();
-            rootNode.set(elementName, firstNode);
-            exceptArrayElementValue(smlStr, indexHolder, length, sb, firstNode);
-        }
-        return rootNode;
-    }
-
-    private static void exceptArrayElementValue(String smlStr, MutableInt indexHolder, int length, StringBuilder sb, ArrayNode firstNode) {
-        int index = indexHolder.getValue();
-        // tow model,primitive type or container type
-        while (index < length) {
-
-        }
-        indexHolder.setValue(index);
-    }
-
-    private static void exceptObjectElementValue(String smlStr, MutableInt indexHolder, int length, StringBuilder sb, ObjectNode firstNode) {
-    }
-
-    private static char readString(String smlStr, MutableInt indexHolder, int length, StringBuilder sb) {
-        int index = indexHolder.getValue();
-        char c0 = 0;
-        int count = 0;
-        while (index < length) {
-            c0 = smlStr.charAt(index++);
-            if (!Character.isWhitespace(c0)) {
-                sb.append(c0);
-                count++;
-            } else {
-                //meet whitespace again
-                if (count != 0) {
-
-                }
-            }
-        }
-        return c0;
-    }
-
-    private static int exceptElementName(String smlStr, MutableInt indexHolder, int length, StringBuilder sb) {
-        int index = indexHolder.getValue();
-        char c0;
-        int model = 0;
-        while (index < length) {
-            c0 = smlStr.charAt(index++);
-            if (c0 == LEFT_PARENTHESIS) {
-                model = 1;
-                break;
-            }
-            if (c0 == LEFT_BRACE) {
-                model = 2;
-                break;
-            }
-            if (c0 == LEFT_BRACKET) {
-                model = 3;
-                break;
-            }
-            if (!Character.isWhitespace(c0)) {
-                sb.append(c0);
-            }
-        }
-
-        if (model == 0) {
-            while (index < length) {
-                c0 = smlStr.charAt(index++);
-                if (c0 == LEFT_PARENTHESIS) {
-                    model = 1;
-                    break;
-                }
-                if (c0 == LEFT_BRACE) {
-                    model = 2;
-                    break;
-                }
-                if (c0 == LEFT_BRACKET) {
-                    model = 3;
-                    break;
-                }
-                if (!Character.isWhitespace(c0)) {
-                    throw new SmlFormatException(EXCEPT_LEFT);
-                }
-            }
-        }
-        indexHolder.setValue(index);
-        return model;
-    }
-
-    private static void exceptAttribute(String smlStr, MutableInt indexHolder, int length, StringBuilder sb, ObjectNode objectNode) {
-        int index = indexHolder.getValue();
-        char c0 = 0;
-        int count = 0;
-        boolean hasEqualSign = false;
-        // loop find attribute
-        attributeEnd:
-        while (index < length) {
-            //find attribute name
-            while (index < length) {
-                c0 = smlStr.charAt(index++);
-                if (c0 == RIGHT_PARENTHESIS) {
-                    if (count != 0) {
-                        throw new SmlFormatException(ERROR_ATTRIBUTE);
-                    }
-                    break attributeEnd;
-                }
-                if (c0 == EQUAL_SIGN) {
-                    if (count == 0) {
-                        throw new SmlFormatException(EXCEPT_ATTRIBUTE_NAME);
-                    }
-                    hasEqualSign = true;
-                    break;
-                }
-                if (!Character.isWhitespace(c0)) {
-                    sb.append(c0);
-                    count++;
-                } else {
-                    if (count != 0) {
-                        break;
-                    }
-                }
-            }
-            String attributeName = sb.toString();
-            sb.delete(0, count);
-            count = 0;
-            if (!hasEqualSign) {
-                //find equal
-                while (index < length) {
-                    c0 = smlStr.charAt(index++);
-                    if (c0 == EQUAL_SIGN) {
-                        break;
-                    }
-                }
-                if (c0 != EQUAL_SIGN) {
-                    throw new SmlFormatException(EXCEPT_EQUAL);
-                }
-            } else {
-                hasEqualSign = false;
-            }
-            // find attribute value
-            valueEnd:
-            while (index < length) {
-                c0 = smlStr.charAt(index++);
-                if (c0 == SINGLE_QUOTE) {
-                    while (index < length) {
-                        c0 = smlStr.charAt(index++);
-                        if (c0 != SINGLE_QUOTE) {
-                            sb.append(c0);
-                            count++;
-                        } else {
-                            break valueEnd;
-                        }
-                    }
-                }
-                if (c0 == DOUBLE_QUOTE) {
-                    while (index < length) {
-                        c0 = smlStr.charAt(index++);
-                        if (c0 != DOUBLE_QUOTE) {
-                            sb.append(c0);
-                            count++;
-                        } else {
-                            break valueEnd;
-                        }
-                    }
-                }
-                if (!Character.isWhitespace(c0)) {
-                    throw new SmlFormatException(EXCEPT_QUOTE);
-                }
-            }
-            String attributeValue = sb.toString();
-            sb.delete(0, count);
-            count = 0;
-            objectNode.put(attributeName, attributeValue);
-        }
+        return null;
     }
 
 }
